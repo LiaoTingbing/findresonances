3f31d2a464bf4824dd0f975227782bf3a8ccf9b4
ltb <ltb@DESKTOP-FO8H3TR>
2025-05-04 17:46:17 +0800

    忽略 `.vs` 文件夹并修复代码格式问题
    在 `.gitignore` 中添加了 `.vs` 文件夹以避免提交用户特定文件，同时调整了 `findresonances` 函数调用的格式，提升代码可读性。

--- src/main.cpp
+++ src/main.cpp
@@ -8,7 +8,6 @@ using namespace std;
 
 int main()
 {
-
 	double f1 = 1.765;
 	double alpha1 = 0.005;
 	double ampl1 = 1.3;
@@ -24,7 +23,7 @@ int main()
 	//cx_vec signal_short = ampl1 * exp(-IU*(2.0 * PI * f1 * t_short - phase1)) % exp(-alpha1 * t_short) + ampl2 * cos(2 * PI * f2 * t_short - phase2) % exp(-alpha2 * t_short);
 	//signal_short.print();
 	auto t1 = clock();
-	findresonances(signal_long, t_long, vec{ 0,100 },1000,1e-5 ,1000,1e-10);
+	findresonances(signal_long, t_long, vec{0, 100}, 1000, 1e-5, 1000, 1e-10);
 	auto t2 = clock();
 	cout << "\t耗时\t" << double(t2 - t1) / CLOCKS_PER_SEC << endl;
 	return 0;
